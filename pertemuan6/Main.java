302663a9637f229b772c5f3768a1bd43205c9474
Radityae383 <radityaeka383@gmail.com>
2025-12-02 16:51:00 +0700

    Add files via upload

---
+++ pertemuan6/Main.java
@@ -0,0 +1,18 @@
+package pertemuan6;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // membuat objek dari class User
+        User dian = new User();
+
+        // menggunakan method setter
+        dian.setUsername("dian");
+        dian.setPassword("kopiJava");
+
+        // menggunakan method getter
+        System.out.println("Username: " + dian.getUsername());
+        System.out.println("Password: " + dian.getPassword());
+    }
+}
---
+++ pertemuan6/User.java
@@ -0,0 +1,23 @@
+class User {
+
+    private String username;
+    private String password;
+
+    // Setter
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Getter
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
