8fa5053822ca969cbda11e36b4649d18aef8eec7
Radityae383 <radityaeka383@gmail.com>
2025-12-02 16:48:32 +0700

    Add files via upload

---
+++ pertemuan5/Anjing.java
@@ -0,0 +1,13 @@
+public class Anjing extends Hewan {
+
+    double biayaPerawatan;
+
+    void beriPerawatan(double biaya) {
+        biayaPerawatan = biaya;
+    }
+
+    void tampilTambahan() {
+        System.out.println("Biaya Perawatan : " + biayaPerawatan);
+        System.out.println("Total Biaya     : " + hitungTotal(0, biayaPerawatan));
+    }
+}
---
+++ pertemuan5/Hewan.java
@@ -0,0 +1,23 @@
+public class Hewan {
+
+    protected String nama;
+    protected String jenis;
+
+    public Hewan() {
+    }
+
+    public void daftarHewan(String nama, String jenis) {
+        this.nama = nama;
+        this.jenis = jenis;
+    }
+
+    public void tampilkanData() {
+        System.out.println("Nama Hewan : " + nama);
+        System.out.println("Jenis      : " + jenis);
+    }
+
+    // method hitungTotal menerima double
+    public double hitungTotal(double a, double b) {
+        return a + b;
+    }
+}
