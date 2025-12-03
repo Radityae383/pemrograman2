fede0e3c3f12e91eebaee3ab32b4280acb8d9b43
Radityae383 <radityaeka383@gmail.com>
2025-12-03 12:01:02 +0700

    Add files via upload

---
+++ Belajar_GUI/src/main/java/com/mycompany/belajar_gui/Belajar_GUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.belajar_gui;
+
+/**
+ *
+ * @author ACER
+ */
+public class Belajar_GUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
