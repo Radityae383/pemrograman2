aa6400d54f4f5897e49faa55dc3a2b5b0dd41c1d
Radityae383 <radityaeka383@gmail.com>
2026-01-24 00:34:13 +0700

    Add files via upload

---
+++ Pertemuan8/Belajar_GUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Pertemuan8;
+
+/**
+ *
+ * @author ACER
+ */
+public class Belajar_GUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
